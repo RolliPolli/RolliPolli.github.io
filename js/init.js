76a957705a8df6c504236efcbb4c05544b29ae41
RolliPolli <christipersonal@Christis-MacBook-Pro.local>
2017-04-05 23:54:44 -0500

    updating JS to newer jQuery and updated init

--- js/init.js
+++ js/init.js
@@ -352,3 +352,25 @@
 		    
 		    lastScrollTop = st;
 		}
+
+		// This is the 'slick' carousel
+		$(document).on('ready', function() {
+		      $(".regular").slick({
+		        dots: true,
+		        infinite: true,
+		        slidesToShow: 1,
+		        slidesToScroll: 1
+		      });
+		      // $(".center").slick({
+		      //   dots: true,
+		      //   infinite: true,
+		      //   centerMode: true,
+		      //   slidesToShow: 3,
+		      //   slidesToScroll: 3
+		      // });
+		      // $(".variable").slick({
+		      //   dots: true,
+		      //   infinite: true,
+		      //   variableWidth: true
+		      // });
+		    });
